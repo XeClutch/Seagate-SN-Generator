8645fc70ca6c4527a9cbedb7ee9938ef9698bc98
XeClutch <anonxero@yahoo.com>
2015-07-31 23:11:23 -0400

    Update 1.1
    Drives are now loaded from the Drives folder in the same path as the
    executable. Inside the Drives folder will be text documents named the
    part number for the drive it represents.

--- Seagate SN Generator/Program.cs
+++ Seagate SN Generator/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,17 +10,22 @@ namespace Seagate_SN_Generator
 {
     class Program
     {
-        static SeagateDrive ST1000NM0033 = new SeagateDrive("Z1W{#0-2}{ARND4}", SeagateDrive.DriveInterface.SATA, "Constellation ES.3 Internal Drive 9ZM173-003", "1TB", SeagateDrive.DriveType.HDD);
-        static SeagateDrive ST2000NM0033 = new SeagateDrive("Z1X{#0-2}{ARND4}", SeagateDrive.DriveInterface.SATA, "Constellation ES.3 Internal Drive 9ZM175-003", "2TB", SeagateDrive.DriveType.HDD);
-        static SeagateDrive ST3000NM0033 = new SeagateDrive("Z1Y{#0-2}{ARND4}", SeagateDrive.DriveInterface.SATA, "Constellation ES.3 Internal Drive 9ZM178-003", "3TB", SeagateDrive.DriveType.HDD);
-        static SeagateDrive ST4000NM0033 = new SeagateDrive("Z1Z{#0-2}{ARND4}", SeagateDrive.DriveInterface.SATA, "Constellation ES.3 Internal Drive 9ZM170-003", "4TB", SeagateDrive.DriveType.HDD);
-        static SeagateDrive ST4000DX001 = new SeagateDrive("Z301{#3-5}{NRND1}{CRND2}", SeagateDrive.DriveInterface.SATA, "Hybrid Internal Drive 1CE168-300", "4TB", SeagateDrive.DriveType.SSHD);
-        static SeagateDrive[] drives = { ST1000NM0033, ST2000NM0033, ST3000NM0033, ST4000NM0033, ST4000DX001 };
-
         static void Main(string[] args)
         {
+            string[] files = Directory.GetFiles("Drives");
+            SeagateDrive[] drives = new SeagateDrive[files.Length];
+            for (int i = 0; i < files.Length; i++)
+            {
+                string[] lines = File.ReadAllLines(files[i]);
+                SeagateDrive.DriveType type;
+                if (lines[4].Contains("HDD")) type = SeagateDrive.DriveType.HDD;
+                else if (lines[4].Contains("SSD")) type = SeagateDrive.DriveType.SSD;
+                else type = SeagateDrive.DriveType.SSHD;
+                drives[i] = new SeagateDrive(lines[0].Replace("Algorithm: ", ""), (lines[1] == "Interface: SATA" ? SeagateDrive.DriveInterface.SATA : SeagateDrive.DriveInterface.IDE), lines[2].Replace("Name: ", "") + " " + files[i].Replace("Drives\\", "").Replace(".txt", ""), lines[3].Replace("Size: ", ""), type);
+            }
+
             ConsoleUtils.Title = "Seagate SN Generator";
-            ConsoleUtils.PrintText("Seagate SN Generator 0.1\n", true);
+            ConsoleUtils.PrintText("Seagate SN Generator 1.1\n", true);
             ConsoleUtils.PrintText("Please choose a product:", true);
             for (int i = 0; i < drives.Length; i++)
                 ConsoleUtils.PrintText(string.Format("  [{0}] {1} ({2}, {3}, {4})", (i + 1), drives[i].Name, Enum.GetName(typeof(SeagateDrive.DriveInterface), drives[i].Interface), Enum.GetName(typeof(SeagateDrive.DriveType), drives[i].Type), drives[i].Size), true);
